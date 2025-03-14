67e3a11d810d834a2751cc270578af5d23d6633d
WangYu <3590135199@qq.com>
2025-03-14 20:26:31 +0800

    修改目录

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 该仓库用于记录Python练习案例
 
 - Original created in Mar 14th. 
+
+
--- 金融大数据分析/case3.py
+++ 金融大数据分析/case3.py
@@ -6,7 +6,7 @@ close_price = 0
 total_price = 0
 stock=[]
 filtered_stocks=[]
-with open ("stock_prices.txt","r",encoding="UTF-8") as file:
+with open ("../stock_prices.txt", "r", encoding="UTF-8") as file:
         lines = file.readlines()
         for line in lines:
                 parts = line.split()
@@ -19,5 +19,5 @@ with open ("stock_prices.txt","r",encoding="UTF-8") as file:
         for stock,price in stock:
                 if price > 500:
                         filtered_stocks=[stock,price]
-with open ("filtered_stocks.txt","w",encoding="utf-8") as file:
+with open ("filtered_stocks.txt", "w", encoding="utf-8") as file:
         lines = file.writelines()
